5bd3756c2044a124921779435e0f6709122f8c08
Mateusz Dziura <mateusz.dziura@assecobs.pl>
2026-01-03 15:21:55 +0100

    Added meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,8 +69,32 @@ export default function RootLayout({
       <head>
         <meta name="facebook-domain-verification" content="h5mvdptwfsa2ycgvwbcnxkoag2a0d9" />
         <LocalBusinessSchema />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1418423889808655');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </head>
       <body className="antialiased w-full">
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1418423889808655&ev=PageView&noscript=1"
+          />
+        </noscript>
         <div className="w-full min-h-screen flex flex-col">
           <Header />
           <main className="flex-1 w-full">{children}</main>
