758500202025eb12bd7aed3b4b9f3d3072de0b06
Mateusz Dziura <mateusz.dziura@assecobs.pl>
2026-01-04 18:53:26 +0100

    Added new pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,7 +80,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1418423889808655');
+              fbq('init', '2655414831491427');
               fbq('track', 'PageView');
             `,
           }}
@@ -92,7 +92,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1418423889808655&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=2655414831491427&ev=PageView&noscript=1"
           />
         </noscript>
         <div className="w-full min-h-screen flex flex-col">
